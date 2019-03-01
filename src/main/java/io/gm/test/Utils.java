179e6dfa83f30c1c896fb01a227a0a95f0b34c39
gmartov <gmartov@gmail.com>
2019-03-01 14:30:21 -0600

    Add: RegExp/getMatches - The method provides a recursive case insensitive search Add: RegExp/replaceAll - replacement may be string or Function result. Add: Utils - several utils for simplify work with arrays

---
+++ src/main/java/io/gm/test/Utils.java
@@ -0,0 +1,31 @@
+package io.gm.test;
+
+import java.util.List;
+
+public class Utils {
+
+    /**
+     * Return requested element of specified array or null if array does not contain such element
+     *
+     * @param num  - requested number of array element. -1 means the last element.
+     * @param list - array
+     * @param <E>  - type of element
+     * @return element od array or null
+     */
+    public static <E> E getOrNull(int num, List<E> list) {
+        final int size = list.size();
+        if (size > 0 && size > num) {
+            return list.get(num == -1 ? size - 1 : num);
+        } else {
+            return null;
+        }
+    }
+
+    public static <T> boolean isEmpty(T... arrayOfAny) {
+        return arrayOfAny == null || arrayOfAny.length == 0;
+    }
+
+    public static <T> T defaultOrFirst(T _default, T... arrayOfAny) {
+        return isEmpty(arrayOfAny) ? _default : arrayOfAny[0];
+    }
+}
